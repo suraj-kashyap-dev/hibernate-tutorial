0e1499e8b8899bb9cb5cacbe9ddc4dd27b16aac9
my name <laxmi@gmail.com>
2024-12-21 13:36:50 +0530

    load the data from the database.

--- src/main/java/org/erp/Main.java
+++ src/main/java/org/erp/Main.java
@@ -2,29 +2,40 @@ package org.erp;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.erp.Models.Address;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("App is starting....");
-
         Configuration configuration = new Configuration();
+
         SessionFactory sessionFactory = configuration.configure().buildSessionFactory();
 
-        Address address = new Address();
-        address.setCity("Noida");
-        address.setCountry("India");
-        address.setStree1("Sec 62, ARV PARK");
-        address.setStreet2("Sec 62, ARV PARK");
+        Session session = sessionFactory.openSession();
 
-        try (Session session = sessionFactory.openSession()) {
-            Transaction tx = session.beginTransaction();
-            session.persist(address);
-            tx.commit();
-        }
+        Address address = session.get(Address.class, 1);
 
         System.out.println(address);
+
+        sessionFactory.close();
+//        Storing the data into the database
+//        System.out.println("App is starting....");
+//
+//        Configuration configuration = new Configuration();
+//        SessionFactory sessionFactory = configuration.configure().buildSessionFactory();
+//
+//        Address address = new Address();
+//        address.setCity("Noida");
+//        address.setCountry("India");
+//        address.setStree1("Sec 62, ARV PARK");
+//        address.setStreet2("Sec 62, ARV PARK");
+//
+//        try (Session session = sessionFactory.openSession()) {
+//            Transaction tx = session.beginTransaction();
+//            session.persist(address);
+//            tx.commit();
+//        }
+//
+//        System.out.println(address);
     }
 }
